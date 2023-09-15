711934276361cf8709cf0a951a3e2347f4acf540
DongokYang <dyang2@academic.rrc.ca>
2023-09-15 09:10:48 -0500

    added readme file with description

--- module_02.py
+++ module_02.py
@@ -43,3 +43,73 @@ print(f"The number is {number:05}.")
 name = "John" 
 print(f"Hello, {name:>10}.") 
 # output: Hello,      John.
+
+# Standard Operators
+ 
+# +
+result = 5 + 5
+full_name = "John" + " " + "Doe"
+print(result)
+print(full_name)
+# -
+result = 10 - 5
+print(result)
+# *
+result = 14 * 3
+print(result)
+ 
+# / (division)
+result = 42 / 4
+print("/", result)
+ 
+# // (floor division or integer division)
+result = 42 // 4
+print("//", result)
+ 
+# % (modulus)
+result = 100 % 55
+print("%", result)
+ 
+# ** (power)
+result = 2 ** 4
+print("**", result)
+ 
+# Shortcut Operators
+ 
+# +=
+age = age + 1
+age += 1
+print(age)
+# -=
+countdown = 10
+countdown -= 1
+print(countdown)
+# *=
+factor = 12
+factor *= 10
+print(factor)
+ 
+# /=
+half = 41
+half /= 2
+print(half)
+# //=
+less_precise_half = 41
+less_precise_half //= 2
+print(less_precise_half)
+# %=
+remainder = 13
+remainder %= 2
+print(remainder)
+# **=
+power = 5
+power **= 3
+print(power)
+ 
+# Order of Operations
+ 
+result = ((10 + 5) * 2) / 3 - 1 
+print(result)
+ 
+result = 10 + 5 * 2 / 3 - 1
+print(result)
